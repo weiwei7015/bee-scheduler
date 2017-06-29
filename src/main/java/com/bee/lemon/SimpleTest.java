add3618eb6a2c5880992df7e47f4e760164828be
weiwei <hello.vivi@qq.com>
2017-06-29 16:51:50 +0800

    优化

--- src/main/java/com/bee/lemon/SimpleTest.java
+++ src/main/java/com/bee/lemon/SimpleTest.java
@@ -1,11 +1,14 @@
 package com.bee.lemon;
 
 
+import org.quartz.TriggerBuilder;
+
 import java.util.regex.Pattern;
 
 public class SimpleTest {
 
     public static void main(String[] args) {
-        System.out.println(Pattern.matches(".* --datafile=\\S+ .*", " --server.port=8080 --datafile=F:/tmp/hsql/scheduler "));
+
+
     }
 }
