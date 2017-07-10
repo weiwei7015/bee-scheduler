39b35e33c9540c688ff09b8b6c4edaff02533069
weiwei <hello.vivi@qq.com>
2017-07-10 11:38:38 +0800

    style

--- admin-node/src/main/resources/public/main.js
+++ admin-node/src/main/resources/public/main.js
@@ -12,7 +12,7 @@ require.config({
         moment: '/public/lib/moment.min',
         text: '/public/lib/text'
     },
-    urlArgs: 'v=2017071002'
+    urlArgs: 'v=2017071003'
 });
 
 require(['vue', 'vue_router', 'vue_resource', 'ELEMENT', 'moment', 'comp/helper-dialog'], function (Vue, VueRouter, VueResource, Elem, moment, helperDialog) {
