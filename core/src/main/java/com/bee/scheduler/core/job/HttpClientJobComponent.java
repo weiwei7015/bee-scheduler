627e0b5577b401f2a8705b281a0d129e8a38dabb
weiwei <hello.vivi@qq.com>
2017-07-11 22:51:31 +0800

    移除httpclient依赖

--- core/src/main/java/com/bee/scheduler/core/job/HttpClientJobComponent.java
+++ core/src/main/java/com/bee/scheduler/core/job/HttpClientJobComponent.java
@@ -1,25 +1,12 @@
 package com.bee.scheduler.core.job;
 
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.bee.scheduler.core.JobExecutionContextUtil;
 import com.bee.scheduler.core.TaskExecutionContext;
 import com.bee.scheduler.core.TaskExecutionLog;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.http.Header;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.message.BasicHeader;
-import org.quartz.JobDetail;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 
-import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * @author weiwei 用于发起HTTP请求
@@ -51,10 +38,7 @@ public class HttpClientJobComponent extends JobComponent {
         return "{\r" +
                 "    url:'',\r" +
                 "    method:'get',\r" +
-                "    timeout:5000,\r" +
-                "    cookies:[\r" +
-                "        {n:'',v:''}\r" +
-                "    ]\r" +
+                "    timeout:5000\r" +
                 "}";
     }
 
@@ -63,50 +47,21 @@ public class HttpClientJobComponent extends JobComponent {
         JSONObject taskParam = context.getTaskParam();
         TaskExecutionLog taskLogger = context.getLogger();
 
-        String uri = taskParam.getString("url");
+        String url = taskParam.getString("url");
         int timeout = taskParam.getIntValue("timeout");
-        String method = taskParam.getString("method");
-        JSONArray cookies = taskParam.getJSONArray("cookies");
+        String method = StringUtils.upperCase(taskParam.getString("method"));
 
 
-        HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
+        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+        connection.setRequestProperty("user-agent", "BeeScheduler");
+        connection.setRequestMethod(method);
+        connection.setConnectTimeout(timeout);
+        connection.setReadTimeout(timeout);
+        connection.connect();
 
-        RequestConfig.Builder requesetConfigBuilder = RequestConfig.custom();
-        requesetConfigBuilder.setConnectTimeout(timeout);
-        RequestConfig requestConfig = requesetConfigBuilder.build();
+        int result = connection.getResponseCode();
 
-        httpClientBuilder.setDefaultRequestConfig(requestConfig);
-
-        CloseableHttpClient client = httpClientBuilder.build();
-
-
-        HttpUriRequest request;
-        if (StringUtils.equalsIgnoreCase("post", method)) {
-            request = new HttpPost(uri);
-        } else {
-            request = new HttpGet(uri);
-        }
-        // 添加cookie
-        StringBuilder cookiesStr = new StringBuilder("");
-        if (cookies != null && cookies.size() > 0) {
-            for (Object cookie : cookies.toArray()) {
-                JSONObject cookieJson = JSONObject.parseObject(cookie.toString());
-                cookiesStr.append(cookieJson.getString("n")).append("=").append(cookieJson.getString("v")).append("; ");
-            }
-            Header cookieHeader = new BasicHeader("Cookie", cookiesStr.toString());
-            request.addHeader(cookieHeader);
-        }
-        try {
-            // 发起请求
-            CloseableHttpResponse response = client.execute(request);
-            taskLogger.info("任务执行成功 -> " + response.getStatusLine());
-        } finally {
-            try {
-                client.close();
-            } catch (IOException e) {
-                taskLogger.error(e.getMessage(), e);
-            }
-        }
+        taskLogger.info("任务执行成功 -> response status code:" + result);
         return true;
     }
 }
