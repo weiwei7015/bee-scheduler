8a0d46a1665a4abf60d386273a74b92b52dbbf01
weiwei <hello.vivi@qq.com>
2017-07-12 14:28:00 +0800

    java doc

--- admin-node/src/main/java/com/bee/scheduler/admin/model/UserLoginSession.java
+++ admin-node/src/main/java/com/bee/scheduler/admin/model/UserLoginSession.java
@@ -4,89 +4,35 @@ import java.io.Serializable;
 import java.util.Date;
 
 public class UserLoginSession implements Serializable {
+    private static final long serialVersionUID = -8172879662124856674L;
 
-	private static final long serialVersionUID = -8172879662124856674L;
-	
-	private Integer shopId;// 店铺id
-	
-	private String shopSn;// 商家编号
-	
-	private Integer sellerId;// 商家id
-	
-	private String name;// 店铺名称
-	
-	private Integer type;// 店铺类型
-	
-	private String account;// 店铺账号
-	
-	private Date loginTime;// 登录时间
-	
-	private Integer uid;// 兼容用户id
-	
-	private String uname;// 兼容用户名称
+    private String name;
 
-	public Integer getShopId() {
-		return shopId;
-	}
+    private String account;
 
-	public void setShopId(Integer shopId) {
-		this.shopId = shopId;
-	}
+    private Date loginTime;
 
-	public String getShopSn() {
-		return shopSn;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setShopSn(String shopSn) {
-		this.shopSn = shopSn;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public Integer getSellerId() {
-		return sellerId;
-	}
+    public String getAccount() {
+        return account;
+    }
 
-	public void setSellerId(Integer sellerId) {
-		this.sellerId = sellerId;
-	}
+    public void setAccount(String account) {
+        this.account = account;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public Date getLoginTime() {
+        return loginTime;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Integer getType() {
-		return type;
-	}
-
-	public void setType(Integer type) {
-		this.type = type;
-	}
-
-	public String getAccount() {
-		return account;
-	}
-
-	public void setAccount(String account) {
-		this.account = account;
-	}
-
-	public Date getLoginTime() {
-		return loginTime;
-	}
-
-	public void setLoginTime(Date loginTime) {
-		this.loginTime = loginTime;
-	}
-
-	public Integer getUid() {
-		return shopId;
-	}
-
-	public String getUname() {
-		return name;
-	}
-
+    public void setLoginTime(Date loginTime) {
+        this.loginTime = loginTime;
+    }
 }
