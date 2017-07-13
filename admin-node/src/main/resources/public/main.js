8af981b27b60c689af974a75f68d07ad1766e5cc
weiwei <hello.vivi@qq.com>
2017-07-13 17:30:50 +0800

    bugfix

--- admin-node/src/main/resources/public/main.js
+++ admin-node/src/main/resources/public/main.js
@@ -12,7 +12,7 @@ require.config({
         moment: '/public/lib/moment.min',
         text: '/public/lib/text'
     },
-    urlArgs: 'v=2017071301'
+    urlArgs: 'v=2017071302'
 });
 
 require(['vue', 'vue_router', 'vue_resource', 'ELEMENT', 'moment', 'comp/helper-dialog', 'comp/task-detail-dialog'], function (Vue, VueRouter, VueResource, Elem, moment, helperDialog, taskDetailDialog) {
