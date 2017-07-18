7fd20cc8c862514690473342a74fec4879807443
weiwei <hello.vivi@qq.com>
2017-07-18 12:38:51 +0800

    调整任务列表显示字段

--- admin-node/src/main/resources/public/main.js
+++ admin-node/src/main/resources/public/main.js
@@ -11,7 +11,7 @@ require.config({
         moment: '/public/lib/moment.min',
         text: '/public/lib/text'
     },
-    urlArgs: 'v=2017071801'
+    urlArgs: 'v=2017071802'
 });
 
 require(['vue', 'vue_router', 'vue_resource', 'ELEMENT', 'moment', 'comp/helper-dialog', 'comp/task-detail-dialog'], function (Vue, VueRouter, VueResource, Elem, moment, helperDialog, taskDetailDialog) {
