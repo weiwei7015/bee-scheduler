53f111f95802a91141ecff2f2219e86c8aa67c96
weiwei <hello.vivi@qq.com>
2017-12-11 23:32:10 +0800

    AAA

--- admin-node/src/main/java/com/bee/scheduler/admin/SimpleTest.java
+++
@@ -1,8 +0,0 @@
-package com.bee.scheduler.admin;
-
-
-public class SimpleTest {
-    public static void main(String[] args) throws Exception {
-        System.out.println("SimpleTest.main");
-    }
-}
