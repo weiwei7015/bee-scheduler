5bbdea638c97bb3f09fdb4b5666d8d91dd764b17
weiwei <weiwei1@u51.com>
2017-12-11 20:12:04 +0800

    构建工具改为maven

--- admin-node/src/main/java/com/bee/scheduler/admin/dao/TaskDao.java
+++ admin-node/src/main/java/com/bee/scheduler/admin/dao/TaskDao.java
@@ -18,13 +18,13 @@ import java.util.*;
 public class TaskDao extends DaoBase {
 
     public Task get(String schedulerName, String name, String group) {
-        String sql = "SELECT t1.TRIGGER_NAME 'name',t1.TRIGGER_GROUP 'group',t2.JOB_CLASS_NAME 'jobComponent',t1.JOB_DATA 'data',t1.DESCRIPTION 'description' FROM BS_TRIGGERS t1 JOIN bs_job_details t2 ON t1.JOB_NAME = t2.JOB_NAME AND t1.JOB_GROUP = t2.JOB_GROUP where t1.SCHED_NAME = ? and t1.TRIGGER_NAME = ? and t1.TRIGGER_GROUP = ?";
+        String sql = "SELECT t1.TRIGGER_NAME 'name',t1.TRIGGER_GROUP 'group',t2.JOB_CLASS_NAME 'jobComponent',t1.JOB_DATA 'data',t1.DESCRIPTION 'description' FROM BS_TRIGGERS t1 JOIN BS_JOB_DETAILS t2 ON t1.JOB_NAME = t2.JOB_NAME AND t1.JOB_GROUP = t2.JOB_GROUP where t1.SCHED_NAME = ? and t1.TRIGGER_NAME = ? and t1.TRIGGER_GROUP = ?";
         return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Task.class), schedulerName, name, group);
     }
 
     public Pageable<Task> query(String schedulerName, String name, String group, String state, int page) {
         List<Object> args = new ArrayList<>();
-        StringBuilder sqlQueryResultCount = new StringBuilder("SELECT COUNT(1) FROM BS_TRIGGERS t1 JOIN bs_job_details t2 ON t1.JOB_NAME = t2.JOB_NAME AND t1.JOB_GROUP = t2.JOB_GROUP");
+        StringBuilder sqlQueryResultCount = new StringBuilder("SELECT COUNT(1) FROM BS_TRIGGERS t1 JOIN BS_JOB_DETAILS t2 ON t1.JOB_NAME = t2.JOB_NAME AND t1.JOB_GROUP = t2.JOB_GROUP");
         StringBuilder sqlQueryResult = new StringBuilder("SELECT t1.TRIGGER_NAME 'name',t1.TRIGGER_GROUP 'group',t1.TRIGGER_TYPE 'triggerType',t2.JOB_CLASS_NAME 'jobComponent',t1.JOB_DATA 'data',t1.TRIGGER_STATE 'state',t1.PREV_FIRE_TIME 'prevFireTime',t1.NEXT_FIRE_TIME 'nextFireTime',t1.START_TIME 'startTime',t1.END_TIME 'endTime',t1.MISFIRE_INSTR 'misfireInstr',t1.DESCRIPTION 'description' FROM BS_TRIGGERS t1 JOIN BS_JOB_DETAILS t2 ON t1.SCHED_NAME = t2.SCHED_NAME AND t1.JOB_NAME = t2.JOB_NAME AND t1.JOB_GROUP = t2.JOB_GROUP");
 
         StringBuilder sqlWhere = new StringBuilder(" WHERE t1.SCHED_NAME = ?");
@@ -97,7 +97,7 @@ public class TaskDao extends DaoBase {
 
     public int queryCount(String schedulerName, String name, String group, String state) {
         List<Object> args = new ArrayList<>();
-        StringBuilder sqlQueryResultCount = new StringBuilder("SELECT COUNT(1) FROM BS_TRIGGERS t1 JOIN bs_job_details t2 ON t1.JOB_NAME = t2.JOB_NAME AND t1.JOB_GROUP = t2.JOB_GROUP");
+        StringBuilder sqlQueryResultCount = new StringBuilder("SELECT COUNT(1) FROM BS_TRIGGERS t1 JOIN BS_JOB_DETAILS t2 ON t1.JOB_NAME = t2.JOB_NAME AND t1.JOB_GROUP = t2.JOB_GROUP");
         StringBuilder sqlWhere = new StringBuilder(" WHERE t1.SCHED_NAME = ?");
         args.add(schedulerName);
         if (name != null) {
