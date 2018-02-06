93e7bbe8f6287f0176cda4a1ab6451cf64ed44b0
weiwei <weiwei1@u51.com>
2018-02-06 15:13:43 +0800

    comment

--- runnable-node/src/main/java/com/bee/scheduler/runnable/SpringApplicationContext.java
+++ runnable-node/src/main/java/com/bee/scheduler/runnable/SpringApplicationContext.java
@@ -7,7 +7,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 /**
- * @author weiwei1
+ * @author weiwei
  */
 @Component
 public class SpringApplicationContext implements ApplicationContextAware {
