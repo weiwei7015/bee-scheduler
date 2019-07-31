fccdf0455506455e1f973c1c20d78dadba76d6cd
weiwei <weiwei1@u51.com>
2019-07-31 19:00:11 +0800

    新增mail组件

--- bee-scheduler-taskmodules/bstm-mail/src/main/java/com/bee/scheduler/taskmodule/MailTaskModule.java
+++ bee-scheduler-taskmodules/bstm-mail/src/main/java/com/bee/scheduler/taskmodule/MailTaskModule.java
@@ -6,8 +6,17 @@ import com.bee.scheduler.core.TaskExecutionContext;
 import com.bee.scheduler.core.TaskExecutionLogger;
 import org.apache.commons.lang3.StringUtils;
 
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.ArrayList;
+import java.util.Properties;
+
 /**
- * @author weiwei 用于发起HTTP请求
+ * @author weiwei 用于发送邮件
  */
 public class MailTaskModule extends AbstractTaskModule {
 
@@ -38,9 +47,17 @@ public class MailTaskModule extends AbstractTaskModule {
     @Override
     public String getParamTemplate() {
         return "{\r" +
-                "    url:'',\r" +
-                "    method:'get',\r" +
-                "    timeout:5000\r" +
+                "    smtp_host:'',\r" +
+                "    smtp_port:'465',\r" +
+                "    smtp_auth:'true',\r" +
+                "    smtp_ssl_enable:'true',\r" +
+                "    from:'xxx@xx.com',\r" +
+                "    recipients_to:'xxx@xx.com,xxx@xx.com',\r" +
+                "    recipients_cc:'xxx@xx.com,xxx@xx.com',\r" +
+                "    subject:'',\r" +
+                "    content:'',\r" +
+                "    account:'',\r" +
+                "    password:''\r" +
                 "}";
     }
 
@@ -48,15 +65,54 @@ public class MailTaskModule extends AbstractTaskModule {
     public boolean run(TaskExecutionContext context) throws Exception {
         JSONObject taskParam = context.getParam();
         TaskExecutionLogger taskLogger = context.getLogger();
+//        String protocol = taskParam.getString("protocol");
+        String protocol = "smtp";
+        String smtpHost = taskParam.getString("smtp_host");
+        Integer smtpPort = taskParam.getInteger("smtp_port");
+        Boolean smtpAuth = taskParam.getBoolean("smtp_auth");
+        Boolean smtpSSLEnable = taskParam.getBoolean("smtp_ssl_enable");
+        String from = taskParam.getString("from");
+        String recipientTo = taskParam.getString("recipients_to");
+        String recipientCc = taskParam.getString("recipients_cc");
+        String subject = taskParam.getString("subject");
+        String content = taskParam.getString("content");
+        String account = taskParam.getString("account");
+        String password = taskParam.getString("password");
 
-        String url = taskParam.getString("url");
-        int timeout = taskParam.getIntValue("timeout");
-        String method = StringUtils.upperCase(taskParam.getString("method"));
+        Properties properties = new Properties();
+        properties.put("mail.transport.protocol", protocol);
+        properties.put("mail.smtp.host", smtpHost);
+        properties.put("mail.smtp.port", smtpPort);
+        properties.put("mail.smtp.auth", smtpAuth);
+        properties.put("mail.smtp.ssl.enable", smtpSSLEnable);
+
+        Message message = new MimeMessage(Session.getInstance(properties));
+        //发件人
+        message.setFrom(new InternetAddress(from));
+        //收件人
+        ArrayList<InternetAddress> recipientToList = new ArrayList<>();
+        for (String address : StringUtils.split(recipientTo, ",")) {
+            recipientToList.add(new InternetAddress(address));
+        }
+        message.setRecipients(Message.RecipientType.TO, recipientToList.toArray(new Address[0]));
+        //抄送人
+        ArrayList<InternetAddress> recipientCcList = new ArrayList<>();
+        for (String address : StringUtils.split(recipientCc, ",")) {
+            recipientCcList.add(new InternetAddress(address));
+        }
+        message.setRecipients(Message.RecipientType.CC, recipientCcList.toArray(new Address[0]));
+        // 邮件标题
+        message.setSubject(subject);
+        // 邮件内容
+        message.setText(content);
+        // 得到邮差对象
+        Transport transport = Session.getInstance(properties).getTransport();
+        // 连接自己的邮箱账户
+        transport.connect(account, password);
+        // 发送邮件
+        transport.sendMessage(message, message.getAllRecipients());
+        transport.close();
 
         return true;
-    }
-
-    public static void main(String[] args) {
-
     }
 }
