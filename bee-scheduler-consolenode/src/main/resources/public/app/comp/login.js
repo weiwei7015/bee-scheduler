f91a49213977b3abb1143b778ef930e8b21502e8
weiwei <hello.vivi@qq.com>
2019-08-05 00:54:03 +0800

    升级ui

--- bee-scheduler-consolenode/src/main/resources/public/app/comp/login.js
+++ bee-scheduler-consolenode/src/main/resources/public/app/comp/login.js
@@ -1,15 +1,35 @@
-define(['text!comp/login.html'], function (tpl) {
-
-
+define(['text!./login.html'], function (tpl) {
     return {
         template: tpl,
         components: {},
         data: function () {
             return {
-                a: "",
-                b: 1
+                account: "",
+                password: "",
+                errorMsg: "",
+                loginProcessing: false
             };
         },
-        methods: {}
+        methods: {
+            login: function () {
+                var vm = this;
+                var form = new FormData();
+                form.append("account", vm.account);
+                form.append("password", vm.password);
+
+                vm.loginProcessing = true;
+                vm.$http.post("/passport/login", form).then(function (re) {
+                    vm.loginProcessing = false;
+                    if (re.body === 1) {
+                        vm.errorMsg = "";
+                        vm.$router.push("/");
+                    } else {
+                        vm.errorMsg = "用户名或密码有误";
+                    }
+                }, function () {
+                    vm.loginProcessing = false;
+                });
+            }
+        }
     };
 });
