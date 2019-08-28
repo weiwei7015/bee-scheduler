b382004bc840873496b847e839ec215d3faeff95
weiwei <weiwei1@u51.com>
2019-08-28 13:42:48 +0800

    调整执行频率限制至30秒/次

--- bee-scheduler-context/src/main/java/com/bee/scheduler/context/listener/VetoDangerousTaskListener.java
+++ bee-scheduler-context/src/main/java/com/bee/scheduler/context/listener/VetoDangerousTaskListener.java
@@ -13,7 +13,7 @@ public class VetoDangerousTaskListener extends TaskListenerSupport {
     @Override
     public boolean vetoJobExecution(Trigger trigger, JobExecutionContext context) {
         int minExecInterval = 1000 * 30;//执行频率限制为30秒
-        //因系统设置的BATCH_TRIGGER_ACQUISITION_FIRE_AHEAD_TIME_WINDOW=5000,此处检查执行频率时允许有5秒的误差
+        //因系统设置的batchTriggerAcquisitionFireAheadTimeWindow=5000,此处检查执行频率时允许有5秒的误差
         int broadMinExecInterval = minExecInterval - 5000;
         if (context.getPreviousFireTime() != null && context.getFireTime().getTime() - context.getPreviousFireTime().getTime() < broadMinExecInterval) {
             logger.warn("任务上次执行时间：" + DateFormatUtils.format(context.getPreviousFireTime(), "yyyy-MM-dd HH:mm:ss") + "，任务频率不能高于" + minExecInterval + "ms/次，请调整任务配置");
